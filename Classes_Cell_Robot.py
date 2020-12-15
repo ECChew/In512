0815c7c6aa0117bafdc4240d1fcf820284b1ffb1
Yoshiinon <62988708+Yoshiinon@users.noreply.github.com>
2020-12-15 18:40:39 +0900

    Add files via upload
    Spread cancelling behind wall

--- Classes_Cell_Robot.py
+++ Classes_Cell_Robot.py
@@ -11,18 +11,26 @@ class Cell:
     #Spread the information of a human 2 cells around
     def Set_Human(self, Map):
         self.L = findMaxL([0, 1, 0, 0], self.L) 
+        self.ListWall = []
+        self.ListHuman = []
         for i in range(-1,2):
             for j in range (-1,2):
                 try:
-                    #print("i - " ,i , "j - ", j ,Map[self.x + i, self.y + j].L)
-                    Map[self.x+i, self.y+j].L = findMaxL(Map[self.x + i, self.y + j].L, [0, 0.6, 0, 0])
+                    if Map[self.x+i, self.y+j].L[0] != 1:           #If wall no spread of human
+                        
+                        #print("i - " ,i , "j - ", j ,Map[self.x + i, self.y + j].L)
+                        Map[self.x+i, self.y+j].L = findMaxL(Map[self.x + i, self.y + j].L, [0, 0.6, 0, 0])
+                    else:
+                        self.ListWall.append([self.x+i, self.y+j])
+                        self.ListHuman.append([self.x, self.y])
                 except:
                     pass
                 
         for i in range(-2,3):
             for j in range (-2,3):
                 try:
-                    Map[self.x+i, self.y+j].L = findMaxL(Map[self.x + i, self.y + j].L, [0, 0.3, 0, 0])
+                    if Map[self.x+i, self.y+j].L[0] != 1: 
+                        Map[self.x+i, self.y+j].L = findMaxL(Map[self.x + i, self.y + j].L, [0, 0.3, 0, 0])
                 except:
                     pass
     # Spread the information of a wall 1 cell around 
@@ -36,9 +44,26 @@ class Cell:
                 except:
                     pass
                 
+    def RemoveThroughWall(self, Map):
+        try:
+            for i in range(len(self.ListHuman)):
+                xval = self.ListHuman[i][0] - self.ListWall[i][0]         # xval = 0, -1, 1 
+                yval = self.ListHuman[i][1] - self.ListWall[i][1]         # yval = 0, -1, 1
+                if (xval == 1 or xval ==-1) and yval == 0:
+                    for j in range(-1,2):
+                        Map[self.ListWall[i][0]-xval, self.ListWall[i][1]+j].L[1] = 0
+                elif xval == 0 and (yval == 1 or yval == -1):
+                    for j in range(-1,2):
+                        Map[self.ListWall[i][0]+j, self.ListWall[i][1]-yval].L[1] = 0
+                else:
+                    Map[self.ListWall[i][0]-xval, self.ListWall[i][1]-yval].L[1] = 0
+        except:
+            pass
     def Set_Robot(self, Map):
         self.L = [0, 0, 1, 1]
         Map[self.x, self.y].L = findMaxL(Map[self.x, self.y].L, self.L)
+        
+
             
 class Believe():
     def __init__(self, xValue, yValue):
@@ -83,7 +108,7 @@ class Believe():
                         try:
                             if MapProba[self.x + i, self.y + j].L[1] == 0:
                                 MapBelief[self.x+i, self.y+j].L[1] *= 1
-                            except: 
+                        except: 
                                 pass
             for i in range(-1,2):
                 for j in range (-1,2):
@@ -105,7 +130,8 @@ class Believe():
 
 
                     
-            
+         
+
             
                 
                 
--- Main_Poo.py
+++ Main_Poo.py
@@ -1,6 +1,7 @@
 import numpy as np
 import Classes_Cell_Robot as cl
-
+import matplotlib.pyplot as plt
+from matplotlib import colors
 GridProba = np.zeros((20,20), dtype = cl.Cell)
 for i in range(20):
     for j in range(20):
@@ -36,16 +37,33 @@ for i in range(11,16):
     GridProba[i,18].Set_Wall(GridProba)
 
 
-#### Position of humans
+#### Position of humans and spread cancellation behind wall
 GridProba[2,4].Set_Human(GridProba)
+GridProba[2,4].RemoveThroughWall(GridProba)
+
 GridProba[0,8].Set_Human(GridProba)
+GridProba[0,8].RemoveThroughWall(GridProba)
+
 GridProba[0,19].Set_Human(GridProba)
+GridProba[0,19].RemoveThroughWall(GridProba)
+
 GridProba[8,11].Set_Human(GridProba)
+GridProba[8,11].RemoveThroughWall(GridProba)
+
 GridProba[8,17].Set_Human(GridProba)
+GridProba[8,17].RemoveThroughWall(GridProba)
+
 GridProba[12,9].Set_Human(GridProba)
+GridProba[12,9].RemoveThroughWall(GridProba)
+
 GridProba[19,8].Set_Human(GridProba)
+GridProba[19,8].RemoveThroughWall(GridProba)
+
 GridProba[19,14].Set_Human(GridProba)
+GridProba[19,14].RemoveThroughWall(GridProba)
+
 GridProba[19,19].Set_Human(GridProba)
+GridProba[19,19].RemoveThroughWall(GridProba)
 
 #### Initial position of the robot
 GridProba[15,1].Set_Robot(GridProba)
@@ -57,9 +75,29 @@ np.save('Map.npy', GridProba)
 
 print(GridProba[1,5].L)
 
+people = np.zeros(GridProba.shape, dtype=float)
+walls = np.zeros(GridProba.shape, dtype=float)
+hybrid = np.zeros(GridProba.shape, dtype=float)
+for i in range(GridProba.shape[0]):
+    for j in range(GridProba.shape[1]):
+        walls[i, j] = GridProba[i, j].L[0]
+        people[i, j] = GridProba[i, j].L[1]
+        hybrid[i, j] = GridProba[i, j].L[1]- GridProba[i, j].L[0]
+
+cmap = colors.ListedColormap(['White','Gray','Black'])
+cmap2 = colors.ListedColormap(['White','Yellow', 'Orange','Red'])
+plt.figure(figsize=(6,6))
+plt.pcolor(walls[::-1,:],cmap=cmap,edgecolors='k', linewidths=3)
+plt.figure(figsize=(6,6))
+plt.pcolor(people[::-1, :],cmap=cmap2,edgecolors='k', linewidths=3)
+plt.figure(figsize=(6,6))
+plt.pcolor(hybrid[::-1, :],cmap='Reds',edgecolors='k', linewidths=3)
+plt.xticks(np.arange(0.5,20.5,step=1))
+plt.yticks(np.arange(0.5,20.5,step=1))
+plt.show()
 
 
-
+print(GridProba[3,3].L)
 
 
 
